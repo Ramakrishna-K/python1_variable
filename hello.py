8fa573c719ed687f061dcdcde1e92d06d6fde828
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-07-04 16:13:22 +0530

    first python file

---
+++ hello.py
@@ -0,0 +1,69 @@
+# # varialble in python
+# #  creating and using variables in pyhton
+
+# age = 24;
+# name = "Ramakrishna";
+
+# print(age);
+# print(name);
+
+# # using variable in computations
+# price = 100;
+# tax = 18;
+
+# total_price = price + tax;
+
+# print(total_price)
+
+# #  how variable work internally
+# x  = 20
+# y= 30
+# z = 23
+# b = z
+# #  if you late change the value of b = 39
+
+# b =39
+# print(x,y,z,b)
+
+# #  Dynamically typed nature of python
+# # In Python, variables do not have fixed types.
+# #  You can assign different types of data to the same variable:
+# x = 10;
+# print(x)
+
+# x ='Ramakrishna'
+# print(x)
+
+# # Boolean variable in python
+# is_valid = True
+# is_published = False
+
+# print(is_valid);
+# print(is_published)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
